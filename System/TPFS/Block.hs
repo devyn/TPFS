53ba9da8863897548cba9a8107930d2b06b15bcf
devyn <devyn.cairns@gmail.com>
2011-09-11 22:10:26 -0700

    Improvements to the Block module. Please read entire commit message.
    + Export list and better documentation.
    
    + nextBlock and consBlock get/set the next block on a block.
    
    + nthBlock traverses a block chain to an index.
    
    + readBlocksFrom starts reading at a specific byte offset within the
      block content chain, intelligently finding the first block instead
      of wasting resources by reading in unneeded bytes.
    
    + overwriteBlock allows writing within a block.
    
    + overwriteBlocks allows writing within a block chain. It will not
      create new blocks, so it returns any excess, which can then be piped
      into createBlocks if desired.
    
    + EndOfChain error added; thrown by readBlocksFrom when the starting
      byte offset is out of the range of the chain.
    
    - readBlock no longer returns the next block -- use nextBlock.
    
    - writeBlock no longer sets the next block -- use consBlock or
      createBlock.
    
    - writeBlocks has been renamed to createBlocks.

--- System/TPFS/Block.hs
+++ System/TPFS/Block.hs
@@ -1,4 +1,21 @@
-module System.TPFS.Block where
+module System.TPFS.Block (
+  -- * Primitive operations
+  -- ** Reading
+  nextBlock,
+  nthBlock,
+  readBlock,
+  readBlocks,
+  readBlocksFrom,
+  -- ** Writing
+  consBlock,
+  writeBlock,
+  createBlock,
+  overwriteBlock,
+  createBlocks,
+  overwriteBlocks,
+  -- * Mathematical helper functions
+  divBlocks
+  ) where
 
 import qualified Control.Exception as E
 import           Data.Binary
@@ -11,64 +28,172 @@ import           System.TPFS.Device
 import           System.TPFS.Errors
 import           System.TPFS.Header
 
+-- | Gets the next block in the chain, if there is one.
+nextBlock :: Device m h
+          => h
+          -> Header
+          -> Address            -- ^ The address of the block.
+          -> m (Maybe Address)  -- ^ The address of the next block, if there is one.
+nextBlock h hdr a = do ea <- dGet h (a + fromIntegral (blockSize hdr - 16)) 16
+                       case decode ea of
+                         0  -> return $ Nothing
+                         a' -> return $ Just a'
+
+-- | Sets the address of the next block in the chain.
+-- 
+-- NB: It doesn't check for 'Just 0', which would be equivalent to
+-- Nothing.
+consBlock :: Device m h
+          => h
+          -> Header
+          -> Address        -- ^ The address of the block to cons.
+          -> Maybe Address  -- ^ The address of the block to cons to, or 'Nothing' for @[]@.
+          -> m ()
+consBlock h hdr a b = dPut h (a + fromIntegral (blockSize hdr - 16)) (encode $ maybe 0 id b)
+
+-- | Attempts to get the nth block of a chain.
+nthBlock :: (Device m h, Integral i)
+         => h
+         -> Header
+         -> Address            -- ^ The block to start at.
+         -> i                  -- ^ The block index from zero to find.
+         -> m (Maybe Address)  -- ^ 'Nothing' if the index is outside
+                               -- the list, or 'Just' the block address.
+nthBlock h hdr a 0 = return $ Just a
+nthBlock h hdr a i = do ma' <- nextBlock h hdr a
+                        maybe (return Nothing) (\ a' -> nthBlock h hdr a' (i-1)) ma'
+
 -- | Reads a single block from the filesystem.
-readBlock        :: Device m h
-                 => h
-                 -> Header
-                 -> Address                        -- ^ The address of the block to read from.
-                 -> m (ByteString, Maybe Address)  -- ^ The contents of the block, and a pointer to the
-                                                   -- next block in the chain if there is one.
-readBlock h hdr a = do bc  <- dGet h  a                (blockSize hdr - 16)
-                       enn <- dGet h (a + fromIntegral (blockSize hdr - 16)) 16
-                       let next = decode enn
-                           in return (bc, if next == 0
-                                             then Nothing
-                                             else Just next)
+readBlock :: Device m h
+          => h
+          -> Header
+          -> Address       -- ^ The address of the block to read from.
+          -> m ByteString  -- ^ The contents of the block.
+readBlock h hdr a = dGet h a (blockSize hdr - 16)
 
 -- | Lazily reads an entire block chain into a bytestring. When used
--- with the IO monad, it's possible that this function is unsafe.
-readBlocks        :: Device m h => h -> Header -> Address -> m ByteString
-readBlocks h hdr a = do (s,n) <- readBlock h hdr a
+-- with the IO monad, this function might break referential
+-- transparency due to interleaving.
+readBlocks :: Device m h
+           => h
+           -> Header
+           -> Address       -- ^ The address of the first block.
+           -> m ByteString  -- ^ The collective contents until the end of the chain,
+                            -- appended lazily.
+readBlocks h hdr a = do s <- readBlock h hdr a
+                        n <- nextBlock h hdr a
                         case n of
                           Nothing -> return s
                           Just a' -> do s' <- interleave $ readBlocks h hdr a'
                                         return $ B.append s s'
 
+-- | Like 'readBlocks', but travels to the first block before
+-- reading. Should make things faster for reading only part of a
+-- file. Throws 'EndOfChain' if the block chain isn't big enough to
+-- reach the offset.
+readBlocksFrom :: (Device m h, Integral i)
+               => h
+               -> Header
+               -> Address       -- ^ Address of the first block from which to calculate the offset.
+               -> i             -- ^ Byte offset to start reading from.
+               -> m ByteString
+readBlocksFrom h hdr a o
+    | o `div` bcs == 0 = B.drop (fromIntegral o) `fmap` readBlocks h hdr a
+    | otherwise = do ma' <- nextBlock h hdr a
+                     case ma' of
+                       Nothing -> E.throw EndOfChain
+                       Just a' -> readBlocksFrom h hdr a' (o - bcs)
+  where bcs = fromIntegral (blockSize hdr - 16)
+
 -- | Writes a bytestring to a block. The string will be truncated to
--- @blockSize hdr - 16@.
+-- @blockSize hdr - 16@. If it is shorter than @blockSize hdr - 16@,
+-- it will be padded with null bytes.
 writeBlock :: Device m h
            => h
-           -> Header                 -- ^ The filesystem header.
-           -> Address                -- ^ The address to place the block at.
-           -> (ByteString, Address)  -- ^ The block content and next block in the chain.
+           -> Header      -- ^ The filesystem header.
+           -> Address     -- ^ The address to place the block at.
+           -> ByteString  -- ^ The block content.
            -> m ()
-writeBlock h hdr a (s,next) =
-  dPut h a $ truncateString (blockSize hdr - 16) s `B.append` encode next
+writeBlock h hdr a s =
+  dPut h a $ truncateString (blockSize hdr - 16) s
+
+-- | Combines 'writeBlock' and 'consBlock'.
+createBlock :: Device m h
+            => h
+            -> Header
+            -> Address
+            -> (ByteString, Maybe Address) -> m ()
+createBlock h hdr a (s,n) = do writeBlock h hdr a s
+                               consBlock  h hdr a n
+
+-- | Writes "inside" a block at an offset. The string will be
+-- truncated to a maximum length (@blockSize hdr - 16@), but if it is
+-- smaller than that, it won't be padded (in contrast to
+-- 'writeBlock').
+overwriteBlock :: (Device m h, Integral i)
+               => h
+               -> Header
+               -> Address       -- ^ The address of the block.
+               -> i             -- ^ The byte offset relative to the start of the block.
+               -> ByteString    -- ^ A string to insert into the block.
+               -> m ByteString  -- ^ The overflow -- that is, the
+                                -- remainder of the string too large
+                                -- to fit within the block.
+overwriteBlock h hdr a o s =
+  do let (as,bs) = B.splitAt (fromIntegral (blockSize hdr - 16) - fromIntegral o) s
+     dPut h (a + fromIntegral o) as
+     return bs
 
 -- | Uses a given list of addresses to split a string into blocks and
 -- write them to the addresses. 'OutOfSpace' will be raised if the
 -- number of addresses in the list is not sufficient to store the
 -- string.
-writeBlocks :: Device m h
-            => h
-            -> Header
-            -> [Address]
-            -> ByteString
-            -> m ()
-writeBlocks h hdr adrs s
+createBlocks :: Device m h
+             => h
+             -> Header
+             -> [Address]   -- ^ A list of addresses to put blocks in, in order of input.
+             -> ByteString  -- ^ The string to fill the blocks with.
+             -> m ()
+createBlocks h hdr adrs s
     | B.null s            = return ()
     | alen == blen        = f adrs (chopString (blockSize hdr - 16) s)
     | otherwise           = E.throw OutOfSpace
-  where f (a:   _) (c:[]) = writeBlock h hdr a (c,0)
-        f (a:n:as) (c:cs) = writeBlock h hdr a (c,n) >> f (n:as) cs
+  where f (a:   _) (c:[]) = createBlock h hdr a (c,Nothing)
+        f (a:n:as) (c:cs) = createBlock h hdr a (c,Just  n) >> f (n:as) cs
         blen              = B.length s `divBlocks` hdr
         alen              = genericLength $ genericTake blen adrs
                               -- not pretty, but it works with infinite lists.
 
+-- | Writes inside a block chain at a byte offset. It won't create new
+-- blocks, so if there are some bytes that couldn't be written within
+-- the block chain, they will be returned.
+overwriteBlocks :: (Device m h, Integral i)
+                => h
+                -> Header
+                -> Address       -- ^ The base block address.
+                -> i             -- ^ The byte offset relative to the base
+                                 -- block address, within block contents.
+                -> ByteString    -- ^ The string to write inside the block
+                                 -- contents, starting from the offset.
+                -> m ByteString  -- ^ The remainder of the string that couldn't
+                                 -- fit within the block chain.
+overwriteBlocks h hdr a o s =
+  do ma' <- nthBlock h hdr a (o `div` bsc)
+     case ma' of
+       Nothing -> return s
+       Just a' -> f a' (o `mod` bsc) s
+  where bsc     = fromIntegral (blockSize hdr - 16)
+        f a o s = do s' <- overwriteBlock h hdr a o s
+                     if B.null s'
+                        then return B.empty
+                        else do ma' <- nextBlock h hdr a
+                                case ma' of
+                                  Nothing -> return s'
+                                  Just a' -> f a' 0 s'
+
 truncateString l s = head (chopString l s)
 
 chopString :: Integral l => l -> ByteString -> [ByteString]
-
 chopString l s =
   case B.length s `compare` len of
     EQ -> [s]
@@ -76,8 +201,9 @@ chopString l s =
     GT -> let (a,b) = B.splitAt len s in a : chopString len b
   where len = fromIntegral l
 
+-- | Divides a number of bytes into the required number of blocks
+-- according to a filesystem header.
 divBlocks :: Integral i => i -> Header -> i
-
 i `divBlocks` hdr
     | r == 0    = q
     | otherwise = q + 1
--- System/TPFS/Errors.hs
+++ System/TPFS/Errors.hs
@@ -1,13 +1,27 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
-module System.TPFS.Errors (OutOfSpace(..)) where
+module System.TPFS.Errors (
+  OutOfSpace(..),
+  EndOfChain(..)
+  ) where
 
 import           Control.Exception (Exception)
 import           Data.Typeable
 
+-- | Thrown when there are not enough blocks available in the
+-- filesystem to satisfy the requirements of a given operation.
 data OutOfSpace = OutOfSpace deriving Typeable
 
 instance Show OutOfSpace where
   showsPrec _ _ = showString "The target filesystem does not have enough free blocks left to perform this operation."
 
 instance Exception OutOfSpace
+
+-- | Thrown when the end of a block chain was reached before it was
+-- expected to end.
+data EndOfChain = EndOfChain deriving Typeable
+
+instance Show EndOfChain where
+  showsPrec _ _ = showString "Reached end of block chain prematurely."
+
+instance Exception EndOfChain
