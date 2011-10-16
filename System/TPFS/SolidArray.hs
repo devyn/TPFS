4872422a0f61f474cc0bc65848a911e61cf9623f
devyn <devyn.cairns@gmail.com>
2011-10-15 18:24:23 -0700

    Documentation for SolidArray

--- System/TPFS/SolidArray.hs
+++ System/TPFS/SolidArray.hs
@@ -5,6 +5,20 @@ module System.TPFS.SolidArray (SolidArray(..)) where
 import System.TPFS.Address
 import System.TPFS.Device
 
+-- | Solid arrays are on-disk arrays of values of type @t@. They can
+-- be read from and written to with random access.
 class SolidArray t where
-  arrRead  :: (Device m h, Integral i) => h -> Address -> (i,i)    -> m [t]
-  arrWrite :: (Device m h, Integral i) => h -> Address -> i -> [t] -> m ()
+  -- | Read an inclusive range from the array.
+  arrRead  :: (Device m h, Integral i)
+           => h        -- ^ The device handle.
+           -> Address  -- ^ The base address of the array.
+           -> (i,i)    -- ^ The range to read from the array.
+           -> m [t]    -- ^ The resulting list of values.
+
+  -- | Write a list to the array at an offset.
+  arrWrite :: (Device m h, Integral i)
+           => h        -- ^ The device handle.
+           -> Address  -- ^ The base address of the array.
+           -> i        -- ^ The offset to begin writing at.
+           -> [t]      -- ^ The list of values to write at the offset.
+           -> m ()
