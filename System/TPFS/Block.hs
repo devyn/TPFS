267915be627ee1a7effca94b322d51704ed01e40
devyn <devyn.cairns@gmail.com>
2012-02-09 19:30:40 -0800

    Further planning and stripping of System.TPFS.Block

--- System/TPFS/Block.hs
+++ System/TPFS/Block.hs
@@ -1,23 +1,33 @@
--- | Functions for dealing with TPFS blocks, extents, and allocation.
+-- | Functions for dealing with TPFS blocks and extents.
+--
+-- This is not a high level interface; aside from providing the block array
+-- structures, it does not deal with block chains. It does, however, provide
+-- allocation, destruction, reading and writing of the individual objects
+-- themselves.
+--
+-- If you are looking for a high level interface, check out the modules for the
+-- the objects you are trying to access, as TPFS blocks simply contain these
+-- objects. Different objects' blocks are often linked together in different
+-- ways as well; some may be simple linked lists instead of dynamic arrays
+-- ('BlockArray's) for space reasons.
 module System.TPFS.Block (
-  -- * Data structures
+  -- * Blocks
   BlockIndex,
+  blockIndexToAddress,
+  addressToBlockIndex,
+  divBlocks,
+  allocateBlocks,
+  allocateBlock,
+  freeBlocks,
+  freeBlock,
+  -- * Block arrays
   BlockArray(..),
-  -- * Reading
   readBlockArray,
-  readBytesFromBlocks,
-  -- * Writing
   writeBlockArray,
   linkBlockArray,
-  -- * Allocation
+  -- * Extents
   allocateExtent,
-  allocateBlocks,
-  freeExtent,
-  freeBlocks,
-  -- * Utility functions
-  divBlocks,
-  blockIndexToAddress,
-  addressToBlockIndex
+  freeExtent
   ) where
 
 import           Control.Applicative
@@ -37,43 +47,9 @@ import           System.TPFS.Errors
 import           System.TPFS.Filesystem
 import           System.TPFS.Header
 
+--- Blocks ---
+
 type BlockIndex = Word64
-
-data BlockArray = BlockArray { blocks    :: Array Word (BlockIndex)
-                             , nextArray :: BlockIndex
-                             }
-
--- | Reads a 'BlockArray' object from disk.
---
--- Please note that this makes no attempt to check whether the object being
--- referenced is really a 'BlockArray'.
-readBlockArray :: Device m h
-               => h
-               -> Header
-               -> BlockIndex   -- ^ Index of the 'BlockArray' object to read.
-               -> m BlockArray -- ^ The read information.
-
-readBlockArray h hdr idx =
-  do ars <- dGet h (blockIndexToAddress hdr idx) (blockSize hdr)
-     let ar = runGet (replicateM (fromIntegral elc) $ getWord64le) ars
-     return $ BlockArray { blocks    = listArray (0, fromIntegral elc-2) (init ar)
-                         , nextArray = last ar
-                         }
-  where elc = blockSize hdr `quot` 8 -- Word64 is 8 bytes
-
-readBytesFromBlocks = undefined
-
-writeBlockArray = undefined
-
-linkBlockArray = undefined
-
-allocateExtent = undefined
-
-allocateBlocks = undefined
-
-freeExtent = undefined
-
-freeBlocks = undefined
 
 -- | Divides a number of bytes into the required number of blocks
 -- according to a filesystem header.
@@ -98,3 +74,45 @@ addressToBlockIndex hdr a
       = toZero $ fromIntegral (quot (a - blockOffset hdr) (fromIntegral $ blockSize hdr)) + 1
   where toZero x | x < 1     = 0
                  | otherwise = x
+
+allocateBlocks = undefined
+
+allocateBlock = undefined
+
+freeBlocks = undefined
+
+freeBlock = undefined
+
+--- Block arrays ---
+
+data BlockArray = BlockArray { blocks    :: Array Word (BlockIndex)
+                             , nextArray :: BlockIndex
+                             }
+
+-- | Reads a 'BlockArray' object from disk.
+--
+-- Please note that this makes no attempt to check whether the object being
+-- referenced is really a 'BlockArray'.
+readBlockArray :: Device m h
+               => h
+               -> Header
+               -> BlockIndex   -- ^ Index of the 'BlockArray' object to read.
+               -> m BlockArray -- ^ The read information.
+
+readBlockArray h hdr idx =
+  do ars <- dGet h (blockIndexToAddress hdr idx) (blockSize hdr)
+     let ar = runGet (replicateM (fromIntegral elc) $ getWord64le) ars
+     return $ BlockArray { blocks    = listArray (0, fromIntegral elc-2) (init ar)
+                         , nextArray = last ar
+                         }
+  where elc = blockSize hdr `quot` 8 -- Word64 is 8 bytes
+
+writeBlockArray = undefined
+
+linkBlockArray = undefined
+
+--- Extents ---
+
+allocateExtent = undefined
+
+freeExtent = undefined
