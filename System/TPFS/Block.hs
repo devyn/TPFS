ca9fe09ca0c7073f9a7a0ce4ed9a8870aceb1777
devyn <devyn.cairns@gmail.com>
2012-02-12 02:57:02 -0800

    Remove `BlockArray` (I now understand how extents work…)

--- System/TPFS/Block.hs
+++ System/TPFS/Block.hs
@@ -23,11 +23,6 @@ module System.TPFS.Block (
   allocateBlock,
   freeBlocks,
   freeBlock,
-  -- * Block arrays
-  BlockArray(..),
-  readBlockArray,
-  writeBlockArray,
-  linkBlockArray,
   -- * Extents
   allocateExtent,
   freeExtent
@@ -128,52 +123,6 @@ allocateBlock = undefined
 freeBlocks = undefined
 
 freeBlock = undefined
-
---- Block arrays ---
-
-data BlockArray = BlockArray { blocks    :: Array Word Address
-                             , nextArray :: Address
-                             }
-
--- | Reads a 'BlockArray' object from disk.
---
--- Please note that this makes no attempt to check whether the object being
--- referenced is really a 'BlockArray'.
-readBlockArray :: Device m h
-               => Filesystem m h
-               -> Address      -- ^ The address of the 'BlockArray' object to read.
-               -> m BlockArray -- ^ The read information.
-
-readBlockArray fs src =
-  do ars <- dGet (fsHandle fs) src (blockSize $ fsHeader fs)
-     let ar = runGet (replicateM (fromIntegral elc) $ getWord64le) ars
-     return $ BlockArray { blocks    = listArray (0, fromIntegral elc-2) (init ar)
-                         , nextArray = last ar
-                         }
-  where elc = blockSize (fsHeader fs) `quot` 8 -- Word64 is 8 bytes
-
--- | Writes a 'BlockArray' object to disk.
-writeBlockArray :: Device m h
-                => Filesystem m h
-                -> Address    -- ^ The address of the block to write to.
-                -> BlockArray -- ^ The 'BlockArray' object to write.
-                -> m ()
-
-writeBlockArray fs dst ary = dPut (fsHandle fs) dst str
-  where str     = runPut $ foldl put (pure ()) (elems $ blocks ary)
-                        >> putWord64le (nextArray ary)
-        put m e = m >> putWord64le e
-
--- | Links a 'BlockArray' object with another 'BlockArray' object on disk,
--- without needing to read and replace the entire object.
-linkBlockArray :: Device m h
-               => Filesystem m h
-               -> Address -- ^ The address of the 'BlockArray' object to modify.
-               -> Address -- ^ The destination for the 'nextArray' field.
-               -> m ()
-
-linkBlockArray fs a b = dPut (fsHandle fs) adr . runPut $ putWord64le b
-            where adr = a + blockSize (fsHeader fs) - 8
 
 --- Extents ---
 
