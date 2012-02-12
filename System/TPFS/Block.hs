db0553be4d284c3267472c3dc3d0ba908a69741d
devyn <devyn.cairns@gmail.com>
2012-02-12 02:55:32 -0800

    Reintroduce `BlockIndex`es, but make them more useful

--- System/TPFS/Block.hs
+++ System/TPFS/Block.hs
@@ -12,7 +12,13 @@
 -- ('BlockArray's) for space reasons.
 module System.TPFS.Block (
   -- * Blocks
+  BlockIndex,
+  blockIndexToAddress,
+  addressToBlockIndex,
+  addressToBlockIndexAndOffset,
   divBlocks,
+  readBlock,
+  writeBlock,
   allocateBlocks,
   allocateBlock,
   freeBlocks,
@@ -46,6 +52,42 @@ import           System.TPFS.Header
 
 --- Blocks ---
 
+type BlockIndex = Word64
+
+-- | Converts a 'BlockIndex' to an 'Address' in the context of a filesystem.
+blockIndexToAddress :: Header
+                    -> BlockIndex
+                    -> Address
+
+blockIndexToAddress hdr idx = blockOffset hdr + fromIntegral (blockSize hdr) * fromIntegral (idx - 1)
+
+-- | Converts an 'Address' to a 'Blockindex' in the context of a filesystem.
+--
+-- Note: This function will floor any addresses to the block boundary; it does
+-- not preserve offsets. See 'addressToBlockIndexAndOffset'.
+addressToBlockIndex :: Header
+                    -> Address
+                    -> BlockIndex
+
+addressToBlockIndex hdr a
+      = toZero $ fromIntegral (quot (a - blockOffset hdr) (fromIntegral $ blockSize hdr)) + 1
+  where toZero x | x < 1     = 0
+                 | otherwise = x
+
+-- | Converts an 'Address' to a 'BlockIndex' and byte offset within the block
+-- in the context of a filesystem.
+--
+-- Note: Undefined when the 'Address' points to somewhere before the beginning
+-- of the block space.
+addressToBlockIndexAndOffset :: Header
+                             -> Address
+                             -> (BlockIndex, Word64)
+
+addressToBlockIndexAndOffset hdr a
+      | a >= blockOffset hdr = (fromIntegral q + 1, fromIntegral r)
+      | otherwise            = undefined
+  where (q, r)               = quotRem (a - blockOffset hdr) (fromIntegral $ blockSize hdr)
+
 -- | Divides a number of bytes into the required number of blocks
 -- according to a filesystem header.
 divBlocks :: Integral i => i -> Header -> i
@@ -54,6 +96,30 @@ i `divBlocks` hdr
     | r == 0    = q
     | otherwise = q + 1
   where (q, r)  = i `quotRem` fromIntegral (blockSize hdr - 16)
+
+-- | Reads an entire block into memory.
+readBlock :: Device m h
+          => Filesystem m h
+          -> BlockIndex
+          -> m ByteString
+
+readBlock fs idx = dGet (fsHandle fs) (blockIndexToAddress (fsHeader fs) idx) (blockSize (fsHeader fs))
+
+-- | Replaces a block with the given string. The string is truncated/padded
+-- with NULs to fit the filesystem's block size.
+writeBlock :: Device m h
+           => Filesystem m h
+           -> BlockIndex
+           -> ByteString
+           -> m ()
+
+writeBlock fs idx str = dPut (fsHandle fs) (blockIndexToAddress (fsHeader fs) idx) padstr
+  where padstr | fromIntegral (B.length str) > blockSize (fsHeader fs)
+                   = B.take (fromIntegral . blockSize $ fsHeader fs) str
+               | fromIntegral (B.length str) < blockSize (fsHeader fs)
+                   = B.append str . flip B.replicate 0 $ fromIntegral (blockSize $ fsHeader fs) - B.length str
+               | otherwise
+                   = str
 
 allocateBlocks = undefined
 
